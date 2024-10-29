c3cbc1ba4cb2fee212d5cc1415cfe1aa918bcc61
Nils Haagen <nhaagen@cat06.de>
2024-10-29 18:02:39 +0100

    UI/Modal/Lightbox: 42416, carousel navigation

--- components/ILIAS/UI/resources/js/Modal/src/Modal.js
+++ components/ILIAS/UI/resources/js/Modal/src/Modal.js
@@ -59,13 +59,12 @@ export default class Modal {
     if (this.#triggeredSignalsStorage[signalData.id] === true) {
       return;
     }
-
     this.#triggeredSignalsStorage[signalData.id] = true;
 
     if (options.ajaxRenderUrl) {
       this.#jquery(component).load(options.ajaxRenderUrl, () => {
         const dialog = component.querySelector('dialog');
-        if(!dialog) {
+        if (!dialog) {
           throw new Error('url did not return a dialog');
         }
         dialog.showModal();
@@ -76,5 +75,76 @@ export default class Modal {
       this.#triggeredSignalsStorage[signalData.id] = false;
     }
     this.#initializedModalboxes[signalData.id] = component.id;
+
+    this.#maybeInitCarousel(component);
+  }
+
+  /**
+   * @param {HTMLDialogElement} component
+   */
+  #maybeInitCarousel(component) {
+    const container = component.querySelector('.carousel-inner');
+    if (!container) {
+      return;
+    }
+
+    const left = component.querySelector('.carousel-control.left');
+    const right = component.querySelector('.carousel-control.right');
+    const indicators = component.querySelectorAll('.carousel-indicators > li');
+
+    left.addEventListener('click', () => this.#nextPage(component, -1));
+    right.addEventListener('click', () => this.#nextPage(component, 1));
+    indicators.forEach(
+      (i) => i.addEventListener(
+        'click',
+        () => this.#gotoPage(component, parseInt(i.getAttribute('data-slide-to'))),
+      ),
+    );
+  }
+
+  #nextPage(component, direction) {
+    const pages = component.querySelectorAll('.carousel-inner > div.item');
+    let index = 0;
+    let current = 0;
+    pages.forEach((p) => {
+      if (p.classList.contains('active')) {
+        current = index;
+      }
+      index += 1;
+    });
+    let next = current + direction;
+    if (next < 0) {
+      next = index - 1;
+    }
+    if (next === index) {
+      next = 0;
+    }
+    this.#gotoPage(component, next);
+  }
+
+  #gotoPage(component, number) {
+    const pages = component.querySelectorAll('.carousel-inner > div.item');
+    const indicators = component.querySelectorAll('.carousel-indicators > li');
+    this.#setActiveInList(pages, number);
+    this.#setActiveInList(indicators, number);
+
+    const title = component.querySelector('h1.modal-title');
+    pages.forEach((item) => {
+      if (item.classList.contains('active')) {
+        title.innerHTML = item.getAttribute('data-title');
+      }
+    });
+  }
+
+  #setActiveInList(list, number) {
+    let index = 0;
+    list.forEach((item) => {
+      if (index !== number) {
+        item.classList.remove('active');
+      } else {
+        item.classList.add('active');
+      }
+      index += 1;
+    });
   }
 }
