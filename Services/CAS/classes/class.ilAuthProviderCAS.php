a3b0fab5d4b0da2f7862c86d1f356a7e951bba8e
ChatGPT <rillke@wikipedia.de>
2024-11-14 13:15:23 +0100

    Make copyright checker happy.

--- Services/CAS/classes/class.ilAuthProviderCAS.php
+++ Services/CAS/classes/class.ilAuthProviderCAS.php
@@ -1,11 +1,5 @@
 <?php
 
-declare(strict_types=1);
-
-// use Monolog\Formatter\LineFormatter;
-// use Monolog\Handler\StreamHandler;
-// use Monolog\Logger;
-
 /**
  * This file is part of ILIAS, a powerful learning management system
  * published by ILIAS open source e-Learning e.V.
@@ -21,6 +15,12 @@ declare(strict_types=1);
  * https://github.com/ILIAS-eLearning
  *
  *********************************************************************/
+
+declare(strict_types=1);
+
+// use Monolog\Formatter\LineFormatter;
+// use Monolog\Handler\StreamHandler;
+// use Monolog\Logger;
 
 /**
  * CAS authentication provider
