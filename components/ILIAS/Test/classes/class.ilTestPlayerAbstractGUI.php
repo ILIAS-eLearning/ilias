3fdd8bf57609d799aadf4e11028bac8bc6eeda92
Lukas Eichenauer <leichenauer@databay.de>
2024-11-25 17:00:10 +0100

    style: improve code readability

--- components/ILIAS/Test/classes/class.ilTestPlayerAbstractGUI.php
+++ components/ILIAS/Test/classes/class.ilTestPlayerAbstractGUI.php
@@ -2762,7 +2762,7 @@ JS;
     protected function getNavigationUrlParameter(): string
     {
         $navigation_url = $this->testrequest->strVal('test_player_navigation_url');
-        if (strlen($navigation_url) > 0) {
+        if (strlen($navigation_url) !== 0) {
             $navigation_url_parts = parse_url($navigation_url);
             $ilias_url_parts = parse_url(ilUtil::_getHttpPath());
 
